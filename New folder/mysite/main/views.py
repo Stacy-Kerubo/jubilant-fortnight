0fc55ac13fd867c911a6314ae7cc468f0529350b
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-01-26 23:45:43 +0300

    Template inheritance

--- New folder/mysite/main/views.py
+++ New folder/mysite/main/views.py
@@ -5,7 +5,7 @@ from .models import ToDoList,Item
 # Create your views here.
 def index(response,id):
     ls=ToDoList.objects.get(id=id)
-    item=ls.item_set.get(id=1)
-    return render(response,"main/base.html",{"name":ls.name})
+    #item=ls.item_set.get(id=3)
+    return render(response,"main/base.html",{})
 def home(response):
     return render(response,"main/home.html",{})
