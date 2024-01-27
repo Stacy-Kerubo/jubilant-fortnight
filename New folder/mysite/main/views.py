95db43f5d3304f10a32a5e276aafd3d06da35e35
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-01-27 20:15:51 +0300

    More on forms

--- New folder/mysite/main/views.py
+++ New folder/mysite/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import ToDoList,Item
+from .forms import Create_new_list
 
 # Create your views here.
 def index(response,id):
@@ -9,3 +10,14 @@ def index(response,id):
     return render(response,"main/list.html",{"ls":ls})
 def home(response):
     return render(response,"main/home.html",{})
+def create(response):
+    if response.method=="POST":
+        form=Create_new_list(response.POST)
+        if form.is_valid():
+            n=form.cleaned_data["name"]
+            t=ToDoList(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+           form=Create_new_list()
+    return render(response,"main/create.html",{"form":form})
