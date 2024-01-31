ac0cebaabf95d6efdfe5401ed630c5c989b1d84e
Stacy Kerubo <152691377+Stacy-Kerubo@users.noreply.github.com>
2024-02-01 00:36:24 +0300

    Adding datetime and sort order to the model

--- DJangoprac3/mysite/blog/models.py
+++ DJangoprac3/mysite/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+#define a Post model that will allow us to store blog posts in the database
+class Post(models.Model):
+    title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250)
+    body=models.TextField()
+    publish= models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering=['-publish']
+    
+     
+    def __str__(self):
+        return self.title
